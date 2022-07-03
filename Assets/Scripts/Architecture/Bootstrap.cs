f6db9594e5caa84678eebe31d00193e729cd4ecc
Export_Nico <auersw.nico@gmail.com>
2022-07-03 21:41:46 +0200

    Adding Initial_Scene, InitSceneLoader and Bootstrap.cs
    Bootstrap loads before any scene load and the Initial Scene Loader can then load the Initial Scene Additive
    ROB-17

---
+++ Assets/Scripts/Architecture/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+namespace Architecture
+{
+    public static class Bootstrap
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void InitializeBootstrap()
+        {
+            var bootstrap = UnityEngine.Object.Instantiate(Resources.Load("P_Bootstrap")) as GameObject;
+            if (bootstrap == null) throw new ApplicationException();
+            UnityEngine.Object.DontDestroyOnLoad(bootstrap);
+        }
+    }
+}
---
+++ Assets/Scripts/Architecture/InitSceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Architecture
+{
+    public class InitSceneLoader : Singleton<InitSceneLoader>
+    {
+        const int INIT_SCENE_BUILDINDEX = 0;
+        protected override void Awake()
+        {
+            base.Awake();
+            Time.timeScale = 0f;
+
+#if UNITY_EDITOR
+            switch (SceneManager.GetActiveScene().buildIndex)
+            {   
+                case INIT_SCENE_BUILDINDEX:
+                    break;
+
+                default:
+                    SceneManager.LoadScene(INIT_SCENE_BUILDINDEX, LoadSceneMode.Additive);
+                    break;
+            }
+#endif
+            Time.timeScale = 1f;
+        }
+    }
+}
