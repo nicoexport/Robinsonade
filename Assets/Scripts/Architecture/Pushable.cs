6fb43dd2311ff080dbeeebac30742a208cf21a85
Export_Nico <auersw.nico@gmail.com>
2022-07-29 12:53:50 +0200

    implement pushing multiple pushables

--- Assets/Scripts/Architecture/Pushable.cs
+++ Assets/Scripts/Architecture/Pushable.cs
@@ -4,15 +4,28 @@ namespace Architecture
 {
    public class Pushable : TileObject
    {
-      public void Push(Direction direction)
+      public bool Push(Direction direction)
       {
          Debug.Log("Push" + direction);
          var target = TileManager.Instance.GetNeighbourPosition(transform.position, direction);
-         if (TileManager.Instance.CheckForTileObject(target) != null || TileManager.Instance.CheckCollision(target))
-            return;
+         if(TileManager.Instance.CheckCollision(target))
+            return false;
+
+         var otherTileObj = TileManager.Instance.CheckForTileObject(target);
+         if (otherTileObj != null)
+         {
+            if (otherTileObj is Pushable otherPushable)
+            {
+               if (!otherPushable.Push(direction))
+               {
+                  return false;
+               }
+            }
+         }
          UnregisterTileObject();
          transform.position = target;
          RegisterTileObject();
+         return true;
       }
    }
 }
