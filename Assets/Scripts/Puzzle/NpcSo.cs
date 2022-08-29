b0edd976b62edb282daee08a90df0cfe73abec73
Export_Nico <auersw.nico@gmail.com>
2022-08-29 13:47:54 +0200

    add npc affections and antipathies

--- Assets/Scripts/Puzzle/NpcSo.cs
+++ Assets/Scripts/Puzzle/NpcSo.cs
@@ -7,6 +7,8 @@ namespace Puzzle
    [CreateAssetMenu(fileName = "New NPC", menuName = "Scriptable Objects/Npc", order = 0)]
    public class NpcSo : ScriptableObject
    {
+      [Header("Facts")]
+      [Space(10)]
       public string Name;
       
       [SerializeField]
@@ -16,14 +18,31 @@ namespace Puzzle
        get => _age;
        set => _age = (int)Mathf.Clamp(value, 0, Mathf.Infinity);
       }
-
+      public ProfessionSo Profession;
+      
+      [Header("Personality")]
+      [Space(10)]
       public PrimaryPersonalityTrait PrimaryPersonalityTrait;
       public SecondaryPersonalityTrait SecondaryPersonalityTrait;
       public TertiaryPersonalityTrait TertiaryPersonalityTrait;
+      [Space(10)] 
+      public List<SymbolCategory> Affections = new List<SymbolCategory>();
+      public List<SymbolCategory> Antipathies = new List<SymbolCategory>();
+
+      [Header("Relationship")] 
+      [Space(10)] 
+      [SerializeField]
+      private float _relationshipLevel;
+      public float RelationshipLevel
+      {
+         get => _relationshipLevel;
+         set => _relationshipLevel = Mathf.Clamp(value, 0f, 100f);
+      }
       
       private void OnValidate()
       {
          Age = _age;
+         RelationshipLevel = _relationshipLevel;
       }
    }
 }
