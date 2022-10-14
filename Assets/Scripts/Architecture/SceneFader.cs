486e1cf799f0ff17156aeebcf78e3123a91a4e45
Krtz_Dev <florian.krtz@gmail.com>
2022-10-14 14:20:27 +0200

    SceneFade

---
+++ Assets/Scripts/Architecture/SceneFader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFader : MonoBehaviour
+{
+    [SerializeField]
+    private SceneFaderUI _sceneFaderUI_prefab;
+
+    private SceneFaderUI _currentSceneFaderUI;
+    private Animator _sceneFaderAnimator;
+
+    private void Awake()
+    {
+        _currentSceneFaderUI = Instantiate(_sceneFaderUI_prefab);
+        _sceneFaderAnimator = _currentSceneFaderUI.GetComponent<Animator>();
+    }
+
+    public float FadeOut()
+    {
+        _sceneFaderAnimator.Play("FadeOut");
+        return _sceneFaderAnimator.GetCurrentAnimatorStateInfo(0).length;
+    }
+
+    public float FadeIn()
+    {
+        _sceneFaderAnimator.Play("FadeIn");
+        return _sceneFaderAnimator.GetCurrentAnimatorStateInfo(0).length;
+    }
+}
--- Assets/Scripts/Architecture/SceneLoader.cs
+++ Assets/Scripts/Architecture/SceneLoader.cs
@@ -1,24 +1,59 @@
 using Architecture;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : Singleton<SceneLoader>
 {
-    private Scene currentScene;
-    private int sceneToLoad;
+    private Scene _currentScene;
+    private int _sceneToLoad;
+
+    private SceneFader _sceneFader;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _sceneFader = GetComponent<SceneFader>();
+    }
 
     public void LoadScene(int sceneToLoad)
     {
-        this.sceneToLoad = sceneToLoad;
-        currentScene = SceneManager.GetActiveScene();
-        SceneManager.UnloadSceneAsync(currentScene);
-        InitSceneLoader.Instance.RemoveFromScenes(currentScene.name);
-        SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive).completed += OnLoadCompleted;
+        _sceneToLoad = sceneToLoad;
+        _currentScene = SceneManager.GetActiveScene();
+        StartCoroutine(FadeOut());
+
+        
+    }
+
+    private IEnumerator FadeOut()
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(_sceneFader.FadeOut());
+        yield return waitTime;
+        UnloadScene();
+    }
+
+    private void UnloadScene()
+    {
+        SceneManager.UnloadSceneAsync(_currentScene).completed += OnUnloadCompleted;
+        InitSceneLoader.Instance.RemoveFromScenes(_currentScene.name);
+    }
+
+    private void OnUnloadCompleted(AsyncOperation asyncOperation)
+    {
+        SceneManager.LoadSceneAsync(_sceneToLoad, LoadSceneMode.Additive).completed += OnLoadCompleted;
     }
 
     private void OnLoadCompleted(AsyncOperation asyncOperation) 
     {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(sceneToLoad));
-        InitSceneLoader.Instance.AddToScenes(SceneManager.GetSceneByBuildIndex(sceneToLoad).name);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(_sceneToLoad));
+        InitSceneLoader.Instance.AddToScenes(SceneManager.GetSceneByBuildIndex(_sceneToLoad).name);
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(_sceneFader.FadeIn());
+        yield return waitTime;
     }
 }
